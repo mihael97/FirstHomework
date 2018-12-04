ebbe7420c2a640d030d6bfafa6ab145bd629138d
Mihael Macuka <mihaelmacuka2@gmail.com>
2018-12-04 21:24:23 +0100

    Rectangle Javadoc changed

--- src/main/java/hr/fer/zemris/java/hw01/Rectangle.java
+++ src/main/java/hr/fer/zemris/java/hw01/Rectangle.java
@@ -6,119 +6,101 @@ import java.util.Locale.Category;
 import java.util.Scanner;
 
 /**
- * Javna klasa koja sluzi kao implementacija racunanja opseg i povrsine iz danih
- * argumenata
- * 
- * @author Mihael
+ * Calculator perimeter and surface of rectangle
  *
+ * @author Mihael
  */
 public class Rectangle {
 
-	/**
-	 * Glavna metoda iz koje se pokrece program
-	 * 
-	 * @param args
-	 *            argumenti-ako je broj argumenata 0,korisnik upisuje dimenzije,ako
-	 *            je 2 ispisuju se podaci pozivanjem metode, inace kraj programa
-	 */
-	public static void main(String[] args) {
-		int length = args.length;
+    /**
+     * Main method
+     *
+     * @param args if number of arguments is 0, user needs to input dimensions, if size of arguments is 2 perimeter and surface will be calculated, otherwise program finishes
+     */
+    public static void main(String[] args) {
+        int length = args.length;
 
-		if (length == 2) {
-			print(Double.parseDouble(args[0]), Double.parseDouble(args[1]));
-			return;
-		} else if (length != 0) {
-			System.err.println("Zadan je krivi broj argumenata!");
-			return;
-		}
+        if (length == 2) {
+            print( Double.parseDouble( args[0] ), Double.parseDouble( args[1] ) );
+            return;
+        } else if (length != 0) {
+            System.err.println( "Zadan je krivi broj argumenata!" );
+            return;
+        }
 
-		try (Scanner sc = new Scanner(System.in)) {
-			double width = 0, height = 0;
+        try (Scanner sc = new Scanner( System.in )) {
+            double width = 0, height = 0;
 
-			do {
-				System.out.println("Unesite sirinu>");
-			} while ((width = check(sc.nextLine())) == 0);
+            do {
+                System.out.println( "Unesite sirinu>" );
+            } while ((width = check( sc.nextLine() )) == 0);
 
-			do {
-				System.out.println("Unesite visinu>");
-			} while ((height = check(sc.nextLine())) == 0);
+            do {
+                System.out.println( "Unesite visinu>" );
+            } while ((height = check( sc.nextLine() )) == 0);
 
-			print(width, height);
-		}
-	}
+            print( width, height );
+        }
+    }
 
-	/**
-	 * Javna metoda koja provjerava nalazi li se argument u odredenom okviru(da li
-	 * je strogo veci od nule). U kodu je stavljeno da se parsiranje u double izvodi
-	 * sukladno trenutnim loklizacijskim postavkama. Lokalizacijske postavke za
-	 * Hrvatsku su takve da se za decimnalni znak koristi decimalni zarez,te ce
-	 * jedino to biti prihvaceno
-	 * 
-	 * @param param
-	 *            - broj u obliku Stringa ciju validnost zelimo provjeriti
-	 * @return 0 ako je strogo manji od jedan,inace duljinu
-	 */
-	private static double check(String param) {
-		try {
-			Locale local = Locale.getDefault(Category.FORMAT);
-			NumberFormat format = NumberFormat.getInstance(local);
-			double number = format.parse(param).doubleValue();
+    /**
+     * Checks if arguments is greater that zero<br>
+     * Method converts number from input depending on current localization settings. For Croatia, <code>decimal comma</code> is in use
+     *
+     * @param param number in String representation
+     * @return 0 if input number is lower or equal to zero or input String cannot be converted to number,otherwise  duljinu
+     */
+    private static double check(String param) {
+        try {
+            Locale local = Locale.getDefault( Category.FORMAT );
+            NumberFormat format = NumberFormat.getInstance( local );
+            double number = format.parse( param ).doubleValue();
 
-			if (number < 0) {
-				System.out.println("Unijeli ste negativnu vrijednost!");
-			} else if (number == 0) {
-				System.out.println("Unijeli ste nulu!");
-			} else {
-				return number;
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println("\'" + param + "\' se ne moze protumaciti kao broj!");
-		}
+            if (number < 0) {
+                System.out.println( "Unijeli ste negativnu vrijednost!" );
+            } else if (number == 0) {
+                System.out.println( "Unijeli ste nulu!" );
+            } else {
+                return number;
+            }
+        } catch (Exception e) {
+            System.out.println( "'" + param + "' se ne moze protumaciti kao broj!" );
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	/**
-	 * Javna metoda koja ispisuje rezultat programa,povrsinu i opseg
-	 * 
-	 * @param width
-	 *            sirina
-	 * @param height
-	 *            visina
-	 */
-	private static void print(double width, double height) {
-		// TODO Auto-generated method stub
-		System.out.println("Pravokutnik sirine " + width + " i visine " + height + " ima povrsinu "
-				+ surface(width, height) + " i opseg " + perimeter(width, height) + ".");
-	}
+    /**
+     * Prints rectangles's perimeter and surface
+     *
+     * @param width  width
+     * @param height height
+     */
+    private static void print(double width, double height) {
+        System.out.println( "Pravokutnik sirine " + width + " i visine " + height + " ima povrsinu "
+                + surface( width, height ) + " i opseg " + perimeter( width, height ) + "." );
+    }
 
-	/**
-	 * Javna metoda koja racuna opseg iz danih argumenata
-	 * 
-	 * @param width
-	 *            sirina
-	 * @param height
-	 *            visina
-	 * @return opseg u obliku Doublea
-	 */
-	private static Double perimeter(double width, double height) {
-		// TODO Auto-generated method stub
-		return 2 * (height + width);
-	}
+    /**
+     * Calculates perimeter from given arguments
+     *
+     * @param width  width
+     * @param height height
+     * @return perimeter in Double format
+     */
+    private static Double perimeter(double width, double height) {
+        return 2 * (height + width);
+    }
 
-	/**
-	 * Javna metoda koja racuna povrsinu iz danih argumenata
-	 * 
-	 * @param width
-	 *            sirina
-	 * @param height
-	 *            visina
-	 * @return povrsinu u obliku Doublea
-	 */
-	private static Double surface(double width, double height) {
-		// TODO Auto-generated method stub
-		return width * height;
-	}
+    /**
+     * Calculates surface from given arguments
+     *
+     * @param width  width
+     * @param height height
+     * @return surface in Double format
+     */
+    private static Double surface(double width, double height) {
+        return width * height;
+    }
 
 }
